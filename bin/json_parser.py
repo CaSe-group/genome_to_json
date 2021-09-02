6d66acbcb05ec7ab9117dd776cda801d193d1c79
dataspott <Ricc.Spott@gmx.de>
2021-09-02 14:01:03 +0200

    Split of tool-parsing into info & results & fixed sourmash-output so that there is not always a "x__" in front of the according taxonomy-level.

--- bin/json_parser.py
+++ bin/json_parser.py
@@ -72,30 +72,44 @@ def sample_id_parsing(OUTPUT_FILE_NAME, HASHID_INPUT):
 	RESULT_FILE.close()
 	return RESULT_FILE
 
-def abricate_parsing(OUTPUT_FILE_NAME, DF_ABRICATE, ABRICATE_DB_VERSION, ANALYZING_DATE):
+def abricate_info_parsing(OUTPUT_FILE_NAME, ABRICATE_DB_VERSION, ANALYZING_DATE):
+	RESULT_FILE = open(OUTPUT_FILE_NAME, "a")
+	RESULT_FILE.write("    \"Abricate_Info\": {\n")
+	RESULT_FILE.write(f"        \"Analysing_Date\": {ANALYSING_DATE},\n")
+	RESULT_FILE.write(f"        \"Abricate_Db_Version\": \"{ABRICATE_DB_VERSION}\",\n")
+	RESULT_FILE.write("    },\n")
+	RESULT_FILE.close()
+	return RESULT_FILE
+
+def abricate_result_parsing(OUTPUT_FILE_NAME, DF_ABRICATE):
 	RES_GENE_LIST = DF_ABRICATE['GENE'].values							#get list of all entries of 'GENE'-column in abricate-dataframe
 	if len(RES_GENE_LIST) == 0:											#check if length of the list = 0
 		RES_GENE_LIST = ['no_resistance_genes']							#if true set variable to single element list
 	RESULT_FILE = open(OUTPUT_FILE_NAME, "a")
-	RESULT_FILE.write("    \"Abricate\": {\n")
-	RESULT_FILE.write(f"        \"Analysing_Date\": {ANALYSING_DATE},\n")
-	RESULT_FILE.write(f"        \"Abricate_Db_Version\": \"{ABRICATE_DB_VERSION}\",\n")
+	RESULT_FILE.write("    \"Abricate_Result\": {\n")
 	[RESULT_FILE.write(f"        \"{RES_GENE}\": \"true\",\n") if RES_GENE != RES_GENE_LIST[-1] else RESULT_FILE.write(f"        \"{RES_GENE}\": \"true\"\n") for RES_GENE in RES_GENE_LIST]
 	#list comprehension over all RES_GENE´s in RES_GENE_LIST -> writes '"RES_GENE": "true",' to RESULT_FILE if not last list-element; else writes '"RES_GENE": "true"' (without comma)
 	RESULT_FILE.write("    },\n")
 	RESULT_FILE.close()
 	return RESULT_FILE
 
-def prokka_parsing(OUTPUT_FILE_NAME, DF_PROKKA, PROKKA_VERSION, ANALYSING_DATE):
+def prokka_info_parsing(OUTPUT_FILE_NAME, PROKKA_VERSION, ANALYSING_DATE):
+	RESULT_FILE = open(OUTPUT_FILE_NAME, "a")
+	RESULT_FILE.write("    \"Prokka_Info\": {\n")
+	RESULT_FILE.write(f"        \"Analysing_Date\": {ANALYSING_DATE},\n")
+	RESULT_FILE.write(f"        \"Prokka_Version\": \"{PROKKA_VERSION}\",\n")
+	RESULT_FILE.write("    },\n")
+	RESULT_FILE.close()
+	return RESULT_FILE
+
+def prokka_result_parsing(OUTPUT_FILE_NAME, DF_PROKKA):
 	PROKKA_GENE_LIST = DF_PROKKA['gene'].values
 	if len(PROKKA_GENE_LIST) == 0:
 			PROKKA_GENE_LIST = ['no_genes_detected']
 	PROKKA_GENE_LIST = PROKKA_GENE_LIST[~pd.isnull(PROKKA_GENE_LIST)]	#select all elements from PROKKA_GENE_LIST that are not null
 	PROKKA_GENE_LIST = np.unique(PROKKA_GENE_LIST)						#remove duplicates from PROKKA_GENE_LIST
 	RESULT_FILE = open(OUTPUT_FILE_NAME, "a")
-	RESULT_FILE.write("    \"Prokka\": {\n")
-	RESULT_FILE.write(f"        \"Analysing_Date\": {ANALYSING_DATE},\n")
-	RESULT_FILE.write(f"        \"Prokka_Version\": \"{PROKKA_VERSION}\",\n")	
+	RESULT_FILE.write("    \"Prokka_Result\": {\n")
 	[RESULT_FILE.write(f"        \"{GENE}\": \"true\",\n") if GENE != PROKKA_GENE_LIST[-1] else RESULT_FILE.write(f"        \"{GENE}\": \"true\"\n") for GENE in PROKKA_GENE_LIST]
 	RESULT_FILE.write("    },\n")
 	RESULT_FILE.close()
@@ -108,14 +122,14 @@ def sourmash_parsing(OUTPUT_FILE_NAME, DF_SOURMASH, ANALYSING_DATE):
 	RESULT_FILE.write(f"        \"Analysing_Date\": {ANALYSING_DATE},\n")
 
 	if STATUS == 'found':
-		TAX_SUPERKINGDOM = DF_SOURMASH['superkingdom'].values[0]
-		TAX_PHYLUM = DF_SOURMASH['phylum'].values[0]
-		TAX_CLASS = DF_SOURMASH['class'].values[0]
-		TAX_ORDER = DF_SOURMASH['order'].values[0]
-		TAX_FAMILY = DF_SOURMASH['family'].values[0]
-		TAX_GENUS = DF_SOURMASH['genus'].values[0]
-		TAX_SPECIES = DF_SOURMASH['species'].values[0]
-		TAX_STRAIN = DF_SOURMASH['strain'].values[0]
+		TAX_SUPERKINGDOM = str(DF_SOURMASH['superkingdom'].values[0])[3:] if str(DF_SOURMASH['superkingdom'].values[0]) != "nan" else str(DF_SOURMASH['superkingdom'].values[0])
+		TAX_PHYLUM = str(DF_SOURMASH['phylum'].values[0])[3:] if str(DF_SOURMASH['phylum'].values[0]) != "nan" else str(DF_SOURMASH['phylum'].values[0])
+		TAX_CLASS = str(DF_SOURMASH['class'].values[0])[3:] if str(DF_SOURMASH['class'].values[0]) != "nan" else str(DF_SOURMASH['class'].values[0])
+		TAX_ORDER = str(DF_SOURMASH['order'].values[0])[3:] if str(DF_SOURMASH['order'].values[0]) != "nan" else str(DF_SOURMASH['order'].values[0])
+		TAX_FAMILY = str(DF_SOURMASH['family'].values[0])[3:] if str(DF_SOURMASH['family'].values[0]) != "nan" else str(DF_SOURMASH['family'].values[0])
+		TAX_GENUS = str(DF_SOURMASH['genus'].values[0])[3:] if str(DF_SOURMASH['genus'].values[0]) != "nan" else str(DF_SOURMASH['genus'].values[0])
+		TAX_SPECIES = str(DF_SOURMASH['species'].values[0])[3:] if str(DF_SOURMASH['species'].values[0]) != "nan" else str(DF_SOURMASH['species'].values[0])
+		TAX_STRAIN = str(DF_SOURMASH['strain'].values[0])[3:] if str(DF_SOURMASH['strain'].values[0]) != "nan" else str(DF_SOURMASH['strain'].values[0])
 
 		RESULT_FILE.write(f"        \"superkingdom\": \"{TAX_SUPERKINGDOM}\",\n")
 		RESULT_FILE.write(f"        \"phylum\": \"{TAX_PHYLUM}\",\n")
@@ -167,14 +181,16 @@ if ABRICATE_INPUT != 'false':
 	ABRICATE_DB_VERSION = ABRICATE_INPUT.split(',')[1]					#split abricate-input by ',' taking the second resulting element
 	DF_ABRICATE = pd.read_csv(ABRICATE_FILE, sep = '\t')				#create pandas-dataframe from abricate-file with tab-stop as separator
 	
-	abricate_parsing(OUTPUT_FILE_NAME, DF_ABRICATE, ABRICATE_DB_VERSION, ANALYSING_DATE)
+	abricate_info_parsing(OUTPUT_FILE_NAME, ABRICATE_DB_VERSION, ANALYSING_DATE)
+	abricate_result_parsing(OUTPUT_FILE_NAME, DF_ABRICATE)
 
 if PROKKA_INPUT != 'false':
 	PROKKA_FILE = PROKKA_INPUT.split(',')[0]
 	PROKKA_VERSION = PROKKA_INPUT.split(',')[1]
 	DF_PROKKA = pd.read_csv(PROKKA_FILE, sep = '\t')
 	
-	prokka_parsing(OUTPUT_FILE_NAME, DF_PROKKA, PROKKA_VERSION, ANALYSING_DATE)
+	prokka_info_parsing(OUTPUT_FILE_NAME, PROKKA_VERSION, ANALYSING_DATE)
+	prokka_result_parsing(OUTPUT_FILE_NAME, DF_PROKKA)
 
 if SOURMASH_INPUT != 'false':
 	DF_SOURMASH = pd.read_csv(SOURMASH_INPUT)
